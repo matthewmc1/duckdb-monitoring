39cd6bf60f7a5684a10c173f4088f3b425578d39
Matthew McGibbon <matthewmcgibbon@Matthews-MacBook-Air.local>
2024-06-25 20:02:44 +0100

    adding note struct and user

--- internal/database.go
+++ internal/database.go
@@ -6,6 +6,7 @@ import (
 	"embed"
 	"fmt"
 	"log/slog"
+	"time"
 
 	"github.com/google/uuid"
 	_ "github.com/marcboeker/go-duckdb"
@@ -13,6 +14,15 @@ import (
 
 //go:embed db/migrations
 var migrations embed.FS
+
+type Note struct {
+	UUID    string `json:"uuid"`
+	User    string `json:"user"`
+	Title   string `json:"title"`
+	Note    string `json:"notes"`
+	Created string `json:"created"`
+	Updated string `json:"updated"`
+}
 
 func duckDB() (*sql.DB, error) {
 	db, err := sql.Open("duckdb", "internal/db/embed.db")
@@ -43,23 +53,52 @@ func CreateNote(ctx context.Context, title, note string) {
 		slog.Info("error creating db")
 	}
 
-	uuid, err := uuid.NewV7()
+	u, err := uuid.NewV7()
 	if err != nil {
 		slog.Info("error creating UUID")
+	}
+
+	us, err := uuid.NewV7()
+	if err != nil {
+		slog.Info("error creating User UUID")
 	}
 
 	txn, err := db.Begin()
 	if err != nil {
 		slog.Info("error starting transactions")
 	}
-	stmt, err := txn.PrepareContext(ctx, fmt.Sprintf("INSERT INTO NOTES (UUID, title, content) VALUES %v, %s, %s", uuid, title, note))
-	if err != nil {
-		slog.Info("error preparing context")
-	}
-	defer stmt.Close()
 
-	if err := txn.Commit(); err != nil {
-		slog.Error("error commitng transaction, rolling back")
+	sql := fmt.Sprintf("INSERT INTO NOTES (UUID, user, title, content, created_at, updated_at) VALUES ('%v', '%s' '%s', '%s', '%s', '%s')", u, us, title, note, time.Now().Format(time.RFC3339), time.Now().Format(time.RFC3339))
+	slog.Info("SQL Query to debug", "query", sql)
+	res, err := txn.ExecContext(ctx, sql)
+	if err != nil {
+		slog.Error("error commitng transaction, rolling back", err)
 		txn.Rollback()
 	}
+
+	slog.Info("commit result", "message", res)
+}
+
+func GetAllNotes(ctx context.Context) Note {
+	db, err := duckDB()
+	note := Note{}
+	if err != nil {
+		slog.Info("error creating db")
+	}
+
+	res, err := db.QueryContext(ctx, "SELECT * FROM Notes")
+	if err != nil {
+		slog.Info("error starting transactions")
+	}
+
+	slog.Info("check for all the notes", "note", res)
+
+	if res.Next() {
+		slog.Info("check for all the notes", "note", res)
+		if err := res.Scan(note.UUID, note.Title, note.Note, note.Created, note.Updated); err != nil {
+			slog.Error("Error deserializing request")
+		}
+		return note
+	}
+	return Note{}
 }
--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	r := gin.Default()
 	internal.CreateDB()
 	r.POST("/v1/create", createNoteHandler)
+	r.GET("/v1/notes", getAllNotes)
 	r.Run(":8080")
 }
 
@@ -19,8 +20,18 @@ func createNoteHandler(c *gin.Context) {
 	ctx := context.Background()
 	value := c.Request.Header.Get("X-Auth-Token")
 	if value != "home" {
-		c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized requqets"})
+		c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized request"})
 	}
 	internal.CreateNote(ctx, "First Note", "Create")
-	c.JSON(http.StatusOK, gin.H{"status": "OK"})
+	c.JSON(http.StatusAccepted, gin.H{"status": "OK"})
+}
+
+func getAllNotes(c *gin.Context) {
+	ctx := context.Background()
+	value := c.Request.Header.Get("X-Auth-Token")
+	if value != "home" {
+		c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized request"})
+	}
+
+	c.JSON(http.StatusOK, internal.GetAllNotes(ctx))
 }
